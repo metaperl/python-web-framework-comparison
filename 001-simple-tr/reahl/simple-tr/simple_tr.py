e460070db6ec839143a0e5c97c467cd4d18d8cee
Iwan Vosloo <iwan@reahl.org>
2018-10-23 07:09:36 +0200

    Simple switch to use @session_scoped

--- 001-simple-tr/reahl/simple-tr/simple_tr.py
+++ 001-simple-tr/reahl/simple-tr/simple_tr.py
@@ -10,11 +10,12 @@ from reahl.web.bootstrap.navbar import Navbar, ResponsiveLayout
 from reahl.web.bootstrap.grid import Container
 from reahl.web.bootstrap.forms import TextInput, Form, FormLayout, Button, ButtonLayout
 from reahl.component.modelinterface import exposed, Field, EmailField, Action, Event
-from reahl.sqlalchemysupport import Session, Base
+from reahl.sqlalchemysupport import Session, Base, session_scoped
 from sqlalchemy import Column, Integer, UnicodeText
 from sqlalchemy.orm import relationship
 
 
+@session_scoped
 class TR(Base):
     __tablename__ = 'simple_tr_tr'
 
@@ -32,10 +33,8 @@ class TR(Base):
     def save(self):
         Session.add(self)
 
-    @classmethod
-    def has_session_data(cls):
-        record = Session.query(__class__).order_by(TR.id.desc()).first()
-        return record
+    def has_data(cls):
+        return self.input_text and self.separator and self.joiner
 
     @exposed('save')
     def events(self, events):
@@ -59,13 +58,12 @@ class MyPage(HTML5Page):
 
 
 class InputForm(Form):
-    def __init__(self, view):
+    def __init__(self, view, tr):
         super(__class__, self).__init__(view, 'address_form')
 
         inputs = self.add_child(FieldSet(view, legend_text='Enter data then click button'))
         inputs.use_layout(FormLayout())
 
-        tr = TR(separator='\\s+', joiner='-')
         inputs.layout.add_input(TextInput(self, tr.fields.input_text))
         inputs.layout.add_input(TextInput(self, tr.fields.separator))
         inputs.layout.add_input(TextInput(self, tr.fields.joiner))
@@ -79,17 +77,17 @@ class MyPanel(Div):
     def __init__(self, view):
         super(__class__, self).__init__(view)
 
-        my_form = InputForm(view)
+        tr = TR.for_current_session()
+        my_form = InputForm(view, tr)
         self.add_child(my_form)
 
-        self.add_child(OutputBox(view, my_form))
+        self.add_child(OutputBox(view, tr))
 
 
 class OutputBox(Widget):
-    def __init__(self, view, form):
+    def __init__(self, view, tr):
         super(__class__, self).__init__(view)
-        tr = TR.has_session_data()
-        if tr is not None:
+        if tr.has_data():
             import re
             new_str = re.sub(tr.separator, tr.joiner, tr.input_text)
             self.add_child(P(view, text=new_str))
